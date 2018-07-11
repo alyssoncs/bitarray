fa507134e256377a7264e507d0cda5aed975c12e
alyssoncs <alysson.cirilo@lsdi.ufma.br>
2018-07-11 00:38:07 -0300

    removed old code

--- bitarray.h
+++ bitarray.h
@@ -1,18 +1,6 @@
 #ifndef _BITARRAY_H_
 #define _BITARRAY_H_
-/*
- * typedef struct
- * {
- *      unsigned char b0 : 1;
- *      unsigned char b1 : 1;
- *      unsigned char b2 : 1;
- *      unsigned char b3 : 1;
- *      unsigned char b4 : 1;
- *      unsigned char b5 : 1;
- *      unsigned char b6 : 1;
- *      unsigned char b7 : 1;
- * } bitnode;
- */
+
 typedef unsigned char bitnode;
 
 bitnode *createBitArray(unsigned long long n);                  //n e' o numero de bits que são necessarios
